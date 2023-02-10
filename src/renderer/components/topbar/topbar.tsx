27e976a5463c903269e7e47fe2a5c8f5e7afed3e
k-Dobosz <kdobosz02@gmail.com>
2023-02-10 18:44:55 +0100

    Add current wifi network to topbar

--- src/renderer/components/topbar/topbar.tsx
+++ src/renderer/components/topbar/topbar.tsx
@@ -2,10 +2,30 @@ import { Link } from 'react-router-dom';
 import { RiNetflixFill, RiYoutubeFill, RiSettings4Fill } from 'react-icons/ri';
 import './topbar.css';
 import clock from 'renderer/utils/clock';
+import { useEffect, useState } from 'react';
+import { RiWifiFill } from 'react-icons/ri';
 
 export default function Topbar() {
+  const { ipcRenderer } = window.electron;
+  const [currentNetwork, setCurrentNetwork] = useState('');
+
+  useEffect(() => {
+    ipcRenderer.sendMessage('wifi-current-request', []);
+
+    ipcRenderer.on('wifi-current', (args: any) => {
+      setCurrentNetwork(args.ssid);
+    });
+  }, [ipcRenderer]);
+
   return (
     <div className="topbar">
+      {currentNetwork != '' ? (
+        <p className="currentNetwork">
+          <RiWifiFill /> {currentNetwork}
+        </p>
+      ) : (
+        ''
+      )}
       <p className="clock">{clock()}</p>
     </div>
   );
